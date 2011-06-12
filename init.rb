949a1bdd0957490f06dbfff04ddcf0480c2140c2
Dai Fujihara <daipresents@gmail.com>
2011-06-12 15:58:35 +0900

    Modified css and added german translation. Thank you Markus.

--- init.rb
+++ init.rb
@@ -3,12 +3,12 @@ require 'redmine'
 Redmine::Plugin.register :redmine_parking_lot_chart do
   name 'Redmine Parking Lot Chart plugin'
   author 'Dai Fujihara'
-  description 'This is a plugin for Redmine'
-  author_url 'http://daipresents.com/weblog/fujihalab/'
-  url 'http://daipresents.com/weblog/fujihalab/archives/2010/03/redmine-parking-lot-chart-plugin.php'
+  description 'Parking lot chart appears in the Agile estimating and planning. This chart makes the theme and the story visible. I try to create redmine plugin for agile development tool.'
+  author_url 'http://daipresents.com/'
+  url 'http://daipresents.com/2010/redmine_parking_lot_chart_plugin/'
 
-  requires_redmine :version_or_higher => '0.9.0'
-  version '0.0.6'
+  requires_redmine :version_or_higher => '1.1.3'
+  version '0.0.7'
 
   project_module :parking_lot_chart do
     permission :parking_lot_chart_view, :parking_lot_chart => :index
